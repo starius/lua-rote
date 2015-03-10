c93ca7bd4107276a15d486b72e2196677425317b
Boris Nagaev <bnagaev@gmail.com>
2015-03-11 00:17:45 +0300

    travis: install rote

---
+++ .travis/install_rote.sh
@@ -0,0 +1,8 @@
+sudo apt-get install libncurses5-dev
+
+wget http://sourceforge.net/projects/rote/files/rote/rote-0.2.8/rote-0.2.8.tar.gz
+tar -xf rote-0.2.8.tar.gz
+cd rote-0.2.8/
+./configure
+make
+sudo make install prefix=/usr
