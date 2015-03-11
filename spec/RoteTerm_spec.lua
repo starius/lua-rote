e024c2676e7a7f4e167648bf8dd75727a42cf63b
Boris Nagaev <bnagaev@gmail.com>
2015-03-11 14:24:32 +0300

    bind some of RoteTerm's methods

---
+++ spec/RoteTerm_spec.lua
@@ -0,0 +1,96 @@
+describe("rote.RoteTerm", function()
+    it("creates RoteTerm", function()
+        local rote = assert(require "rote")
+        local rt = rote.RoteTerm(24, 80)
+    end)
+
+    it("gets size of RoteTerm", function()
+        local rote = assert(require "rote")
+        local rt = rote.RoteTerm(24, 80)
+        assert.equal(24, rt:rows())
+        assert.equal(80, rt:cols())
+    end)
+
+    it("sets cursor to (0, 0) initially", function()
+        local rote = assert(require "rote")
+        local rt = rote.RoteTerm(24, 80)
+        assert.equal(0, rt:row())
+        assert.equal(0, rt:col())
+    end)
+
+    it("sets all chars to space initially", function()
+        local rote = assert(require "rote")
+        local rt = rote.RoteTerm(24, 80)
+        for row = 0, 23 do
+            for col = 0, 79 do
+                assert.equal(' ', rt:cellChar(row, col))
+            end
+        end
+    end)
+
+    it("sets all chars to space initially", function()
+        local rote = assert(require "rote")
+        local rt = rote.RoteTerm(24, 80)
+        for row = 0, 23 do
+            for col = 0, 79 do
+                assert.equal(' ', rt:cellChar(row, col))
+            end
+        end
+    end)
+
+    it("changes cell char", function()
+        local rote = assert(require "rote")
+        local rt = rote.RoteTerm(24, 80)
+        rt:setCellChar(0, 0, 'A')
+        assert.equal('A', rt:cellChar(0, 0))
+    end)
+
+    it("changes cell attribute", function()
+        local rote = assert(require "rote")
+        local rt = rote.RoteTerm(24, 80)
+        rt:setCellAttr(0, 0, 42)
+        assert.equal(42, rt:cellAttr(0, 0))
+    end)
+
+    it("changes current attribute", function()
+        local rote = assert(require "rote")
+        local rt = rote.RoteTerm(24, 80)
+        rt:setAttr(30)
+        assert.equal(30, rt:attr())
+    end)
+
+    it("has child_pid = 0 by default", function()
+        local rote = assert(require "rote")
+        local rt = rote.RoteTerm(24, 80)
+        assert.equal(0, rt:childPid())
+    end)
+
+    it("forks pty", function()
+        local rote = assert(require "rote")
+        local rt = rote.RoteTerm(24, 80)
+        local pid = rt:forkPty('ls')
+        assert.equal(pid, rt:childPid())
+        -- TODO fix zombie
+    end)
+
+    it("forsakes child", function()
+        local rote = assert(require "rote")
+        local rt = rote.RoteTerm(24, 80)
+        rt:forkPty('ls')
+        os.execute('sleep 1')
+        rt:forsakeChild()
+        -- TODO fix zombie
+    end)
+
+    it("gets updates from child", function()
+        local rote = assert(require "rote")
+        local rt = rote.RoteTerm(24, 80)
+        rt:forkPty('ls -a')
+        os.execute('sleep 1')
+        rt:update()
+        rt:forsakeChild()
+        assert.not_equal(' ', rt:cellChar(0, 0))
+    end)
+
+    -- TODO write, inject, keypress, getPtyFd, Snapshot
+end)
--- src/rote.c
+++ src/rote.c
@@ -9,20 +9,225 @@
 
 #include "lua-rote.h"
 
-RoteTerm* lua_RoteTerm_self(lua_State* L, int index) {
+static RoteTerm* lua_RoteTerm_self(lua_State* L, int index) {
     RoteTerm** rt = luaL_checkudata(L, index, "rote_RoteTerm");
     return *rt;
 }
 
-int lua_RoteTerm_gc(lua_State* L) {
+static int lua_RoteTerm_gc(lua_State* L) {
     RoteTerm* rt = lua_RoteTerm_self(L, 1);
     rote_vt_destroy(rt);
     return 0;
 }
 
+static int lua_RoteTerm_rows(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    lua_pushinteger(L, rt->rows);
+    return 1;
+}
+
+static int lua_RoteTerm_cols(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    lua_pushinteger(L, rt->cols);
+    return 1;
+}
+
+static int lua_RoteTerm_row(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    lua_pushinteger(L, rt->crow);
+    return 1;
+}
+
+static int lua_RoteTerm_col(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    lua_pushinteger(L, rt->ccol);
+    return 1;
+}
+
+static void lua_RoteTerm_row_col(lua_State* L,
+        RoteTerm* rt, int* row, int* col) {
+    *row = luaL_checkinteger(L, 2);
+    *col = luaL_checkinteger(L, 3);
+    luaL_argcheck(L, *row >= 0, 2, "row >= 0");
+    luaL_argcheck(L, *row < rt->rows, 2, "row < rows");
+    luaL_argcheck(L, *col >= 0, 3, "col >= 0");
+    luaL_argcheck(L, *col < rt->cols, 3, "col < cols");
+}
+
+// arguments:
+// 1. RowTerm
+// 2. int row
+// 3. int col
+static int lua_RoteTerm_cellChar(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    int row, col;
+    lua_RoteTerm_row_col(L, rt, &row, &col);
+    const char c = rt->cells[row][col].ch;
+    lua_pushlstring(L, &c, 1);
+    return 1;
+}
+
+// arguments:
+// 1. RowTerm
+// 2. int row
+// 3. int col
+// 4. string ch
+static int lua_RoteTerm_setCellChar(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    int row, col;
+    lua_RoteTerm_row_col(L, rt, &row, &col);
+    size_t length;
+    const char* ch = luaL_checklstring(L, 4, &length);
+    luaL_argcheck(L, length == 1, 4, "length(ch) == 1");
+    char c = ch[0];
+    luaL_argcheck(L, c >= 32, 4, "ch >= 32");
+    rt->cells[row][col].ch = c;
+    return 0;
+}
+
+// arguments:
+// 1. RowTerm
+// 2. int row
+// 3. int col
+static int lua_RoteTerm_cellAttr(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    int row, col;
+    lua_RoteTerm_row_col(L, rt, &row, &col);
+    lua_pushinteger(L, rt->cells[row][col].attr);
+    return 1;
+}
+
+static int lua_getAttr(lua_State* L, int index) {
+    int attr = luaL_checkinteger(L, index);
+    luaL_argcheck(L, attr >= 0, index, "attr >= 0");
+    luaL_argcheck(L, attr < 256, index, "attr < 256");
+    return attr;
+}
+
+// arguments:
+// 1. RowTerm
+// 2. int row
+// 3. int col
+// 4. int attr
+static int lua_RoteTerm_setCellAttr(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    int row, col;
+    lua_RoteTerm_row_col(L, rt, &row, &col);
+    int attr = lua_getAttr(L, 4);
+    rt->cells[row][col].attr = attr;
+    return 0;
+}
+
+static int lua_RoteTerm_attr(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    lua_pushinteger(L, rt->curattr);
+    return 1;
+}
+
+// arguments:
+// 1. RowTerm
+// 2. int attr
+static int lua_RoteTerm_setAttr(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    int attr = lua_getAttr(L, 2);
+    rt->curattr = attr;
+    return 0;
+}
+
+static int lua_RoteTerm_childPid(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    lua_pushinteger(L, rt->childpid);
+    return 1;
+}
+
+// arguments:
+// 1. RowTerm
+// 2. command
+// returns:
+// 1. pid
+static int lua_RoteTerm_forkPty(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    const char* command = luaL_checkstring(L, 2);
+    int pid = rote_vt_forkpty(rt, command);
+    lua_pushinteger(L, pid);
+    return 1;
+}
+
+static int lua_RoteTerm_forsakeChild(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    rote_vt_forsake_child(rt);
+    return 0;
+}
+
+static int lua_RoteTerm_update(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    rote_vt_update(rt);
+    return 0;
+}
+
+// arguments:
+// 1. RowTerm
+// 2. string data
+static int lua_RoteTerm_write(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    size_t length;
+    const char* data = luaL_checklstring(L, 2, &length);
+    rote_vt_write(rt, data, length);
+    return 0;
+}
+
+// arguments:
+// 1. RowTerm
+// 2. string data
+static int lua_RoteTerm_inject(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    size_t length;
+    const char* data = luaL_checklstring(L, 2, &length);
+    rote_vt_inject(rt, data, length);
+    return 0;
+}
+
+// arguments:
+// 1. RowTerm
+// 2. int keycode
+static int lua_RoteTerm_keypress(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    int keycode = luaL_checkinteger(L, 2);
+    rote_vt_keypress(rt, keycode);
+    return 0;
+}
+
+static int lua_RoteTerm_getPtyFd(lua_State* L) {
+    RoteTerm* rt = lua_RoteTerm_self(L, 1);
+    int fd = rote_vt_get_pty_fd(rt);
+    lua_pushinteger(L, fd);
+    return 1;
+}
+
+//
+
 static const luaL_Reg RoteTerm_mt[] = {
     {"__gc", lua_RoteTerm_gc},
-    // TODO
+    {"rows", lua_RoteTerm_rows},
+    {"cols", lua_RoteTerm_cols},
+    {"row", lua_RoteTerm_row},
+    {"col", lua_RoteTerm_col},
+    {"cellChar", lua_RoteTerm_cellChar},
+    {"setCellChar", lua_RoteTerm_setCellChar},
+    {"cellAttr", lua_RoteTerm_cellAttr},
+    {"setCellAttr", lua_RoteTerm_setCellAttr},
+    {"attr", lua_RoteTerm_attr},
+    {"setAttr", lua_RoteTerm_setAttr},
+    {"childPid", lua_RoteTerm_childPid},
+    {"forkPty", lua_RoteTerm_forkPty},
+    {"forsakeChild", lua_RoteTerm_forsakeChild},
+    {"update", lua_RoteTerm_update},
+    {"write", lua_RoteTerm_write},
+    {"inject", lua_RoteTerm_inject},
+    {"keypress", lua_RoteTerm_keypress},
+    //{"takeSnapshot", lua_RoteTerm_takeSnapshot},
+    //{"restoreSnapshot", lua_RoteTerm_restoreSnapshot},
+    {"getPtyFd", lua_RoteTerm_getPtyFd},
     {NULL, NULL}
 };
 
@@ -43,6 +248,8 @@ void register_types(lua_State* L) {
 int lua_RoteTerm(lua_State* L) {
     int rows = luaL_checkinteger(L, 1);
     int cols = luaL_checkinteger(L, 2);
+    luaL_argcheck(L, rows > 0, 1, "rows > 0");
+    luaL_argcheck(L, cols > 0, 2, "cols > 0");
     RoteTerm** rt = lua_newuserdata(L, sizeof(RoteTerm*));
     luaL_getmetatable(L, "rote_RoteTerm");
     assert(lua_type(L, -1) == LUA_TTABLE);
