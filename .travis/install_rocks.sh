02813ed13fe86f30366475130364933b9a9f49e4
Boris Nagaev <bnagaev@gmail.com>
2015-03-25 10:21:19 +0300

    travis: no need in my updated rock dkjson
    The same package was added to luarocks:
    https://rocks.moonscript.org/modules/dhkolf/dkjson/2.5-2

--- .travis/install_rocks.sh
+++ .travis/install_rocks.sh
@@ -1,5 +1,4 @@
 luarocks install luasec OPENSSL_LIBDIR=/usr/lib/x86_64-linux-gnu --local
-luarocks install https://raw.githubusercontent.com/starius/dkjson/master/dkjson-2.5-1.rockspec --local
 luarocks install busted --local
 luarocks install luaposix --local
 luarocks install luacov --local
