f9085546803c8036ab11a6d4444f33a98b65078d
Boris Nagaev <bnagaev@gmail.com>
2015-03-28 00:57:11 +0300

    install_rote.sh: set prefix with ./configure
    See discussion here: http://habrahabr.ru/post/254089/#comment_8349843
    
    Changing prefix in Makefile (or with make command line arguments) doesn't
    change it in rote-config, other file generated by ./configure
    
    Providing prefix to ./configure is more reliable.

--- .travis/install_rote.sh
+++ .travis/install_rote.sh
@@ -3,9 +3,9 @@ sudo apt-get install libncurses5-dev
 wget http://sourceforge.net/projects/rote/files/rote/rote-0.2.8/rote-0.2.8.tar.gz
 tar -xf rote-0.2.8.tar.gz
 cd rote-0.2.8/
-./configure
+./configure --prefix=/usr
 make
-sudo make install prefix=/usr
+sudo make install
 
 cd ..
 sudo rm -rf rote-0.2.8/
